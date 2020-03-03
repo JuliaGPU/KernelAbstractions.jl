163b21d418d4f4f51b09d08d95747470fc90794e
James Schloss <leios@cyclops.juliacomputing.io>
2020-03-03 18:02:14 -0500

    getting the GPU test to run, now to make a CPU variant

--- test/async_copy.jl
+++ test/async_copy.jl
@@ -1,30 +1,34 @@
-using KernelAbstractions, CuArrays, CUDAnative, CUDAdrv, Test
+using KernelAbstractions, Test, CUDAapi
+if has_cuda_gpu()
+    using CuArrays
+    CuArrays.allowscalar(false)
+end
 
-function main()
-    T = Float32
-    M = 1024
-    N = 1024
+function GPU_copy_test(M)
 
-    A = CuArray(rand(T, M, N))
-    B = CuArray(rand(T, M, N))
+    A = CuArray(rand(Float64, M))
+    B = CuArray(rand(Float64, M))
 
-    a = Array{T}(undef, M, N)
-    #pin!(a)
+    a = Array{Float64}(undef, M)
+    pin!(a)
 
     len = length(A)
 
     copystream = CuStream(CUDAdrv.STREAM_NON_BLOCKING)
     copyevent = recordevent(copystream)
-    copyevent = async_copy!(pointer(A), pointer(B), M*N, stream=copystream)
+    copyevent = async_copy!(pointer(A), pointer(B), M, stream=copystream)
 
     wait(copyevent)
 
-    copyevent = async_copy!(pointer(B), pointer(a), M*N, stream=copystream)
-    copyevent = async_copy!(pointer(a), pointer(A), M*N,
+    copyevent = async_copy!(pointer(a), pointer(B), M,
+                            stream=copystream)
+    copyevent = async_copy!(pointer(A), pointer(a), M,
                             stream=copystream, dependencies=copyevent)
 
     @test isapprox(a, Array(A))
     @test isapprox(a, Array(B))
 end
 
-main()
+M = 1024
+
+GPU_copy_test(M)
