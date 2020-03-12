09584ef9390f8e59469f8bdfcbf571e443ba8318
GiggleLiu <cacate0129@gmail.com>
2020-03-12 11:13:21 +0800

    fix incorrect merge

--- test/test.jl
+++ test/test.jl
@@ -207,9 +207,3 @@ end
     wait(env)
     @test x == [4,4,4]
 end
-
-@testset "CPU dependencies" begin
-    event = Event(CPU())
-    event = kernel_empty(CPU(), 1)(ndrange=1, dependencies=(event))
-    wait(event)
-end
