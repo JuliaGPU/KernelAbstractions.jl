5bd4101fdbad75efc10a6b488ec2f3c2857a3183
Dilum Aluthge <dilum@aluthge.com>
2021-01-02 03:39:35 -0500

    Deploy the docs to GitHub Pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,33 @@
-using Documenter, KernelAbstractions
+using KernelAbstractions
+using Documenter
 
-makedocs(
-    modules = [KernelAbstractions],
-    sitename = "KernelAbstractions",
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true"
+makedocs(;
+    modules=[KernelAbstractions],
+    authors="JuliaGPU and contributors",
+    repo="https://github.com/JuliaGPU/KernelAbstractions.jl/blob/{commit}{path}#L{line}",
+    sitename="KernelAbstractions.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://juliagpu.github.io/KernelAbstractions.jl",
+        assets=String[],
     ),
-    pages = [
+    pages=[
         "Home" => "index.md",
         "Writing kernels" => "kernels.md",
         "Examples" => [
-            "examples/memcopy.md"
-            "examples/memcopy_static.md"
-            "examples/naive_transpose.md"
-            "examples/performance.md"
-            "examples/matmul.md"
-        ],
-        "API"          => "api.md",
+            "examples/memcopy.md",
+            "examples/memcopy_static.md",
+            "examples/naive_transpose.md",
+            "examples/performance.md",
+            "examples/matmul.md",
+        ], # Examples
+        "API" => "api.md",
         "Extras" => [
-            "extras/unrolling.md"
-        ]
-    ],
-    doctest = true
+            "extras/unrolling.md",
+        ], # Extras
+    ], # pages
+)
+
+deploydocs(;
+    repo="github.com/JuliaGPU/KernelAbstractions.jl",
 )
