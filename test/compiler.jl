fa96bef450c40a6e4cbe3b6f8de2440a2de6a390
Valentin Churavy <v.churavy@gmail.com>
2021-03-22 15:16:06 -0400

    avoid overdubbing of literal_pow
    (cherry picked from commit 7318a2cc7a48606b9b8ec1242a01ef020e221d7a)

--- test/compiler.jl
+++ test/compiler.jl
@@ -8,10 +8,19 @@ import KernelAbstractions.NDIteration: NDRange, StaticSize, DynamicSize
     @show A[I...]
 end
 
+@kernel function literal_pow(A)
+    A[1] = 2^11
+end
+
 function compiler_testsuite()
     kernel = index(CPU(), DynamicSize(), DynamicSize())
     iterspace = NDRange{1, StaticSize{(128,)}, StaticSize{(8,)}}();
     ctx = KernelAbstractions.mkcontext(kernel, 1, nothing, iterspace, Val(KernelAbstractions.NoDynamicCheck()))
 
     @test KernelAbstractions.Cassette.overdub(ctx, KernelAbstractions.__index_Global_NTuple, CartesianIndex(1)) == (1,)
+
+    CI, rt = @ka_code_typed literal_pow(CPU())(zeros(Int,1), ndrange=1)
+    # test that there is no invoke of overdub
+    @test !any(stmt->(stmt isa Expr) && stmt.head == :invoke, CI.code)
 end
+
