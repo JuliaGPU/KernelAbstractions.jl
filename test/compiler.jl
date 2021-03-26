cca68734f1ba6d5435b52db017dfb1047ba8f050
Valentin Churavy <v.churavy@gmail.com>
2021-03-26 15:48:29 -0400

    overdub overflowerr_binaryop

--- test/compiler.jl
+++ test/compiler.jl
@@ -17,6 +17,18 @@ ctx = KernelAbstractions.mkcontext(kernel, 1, nothing, iterspace, Val(KernelAbst
     A[1] = 2^11
 end
 
-CI, rt = @ka_code_typed literal_pow(CPU())(zeros(Int,1), ndrange=1)
-# test that there is no invoke of overdub
-@test !any(stmt->(stmt isa Expr) && stmt.head == :invoke, CI.code)
+@kernel function checked(A, a, b)
+    A[1] = Base.Checked.checked_add(a, b)
+end
+
+let (CI, rt) = @ka_code_typed literal_pow(CPU())(zeros(Int,1), ndrange=1)
+    # test that there is no invoke of overdub
+    @test !any(stmt->(stmt isa Expr) && stmt.head == :invoke, CI.code)
+end
+
+if VERSION >= v"1.5"
+    let (CI, rt) = @ka_code_typed checked(CPU())(zeros(Int,1), 1, 2, ndrange=1)
+        # test that there is no invoke of overdub
+        @test !any(stmt->(stmt isa Expr) && stmt.head == :invoke, CI.code)
+    end
+end
