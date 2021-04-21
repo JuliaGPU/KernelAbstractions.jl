21f5e833a4d24db59fb046dd8bd40c6f7952c0fd
Valentin Churavy <vchuravy@users.noreply.github.com>
2021-04-21 16:46:48 -0400

    CUDAKernels is now 1.6 only as well

--- .ci/develop.jl
+++ .ci/develop.jl
@@ -5,13 +5,14 @@ Pkg.update()
 root_directory = dirname(@__DIR__)
 
 kernelabstractions = Pkg.PackageSpec(path = root_directory)
-cudakernels = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "CUDAKernels"))
 
 Pkg.develop(kernelabstractions)
-Pkg.develop(cudakernels)
 if !(VERSION < v"1.6-")
     rockernels = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "ROCKernels"))
     Pkg.develop(rockernels)
+
+    cudakernels = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "CUDAKernels"))
+    Pkg.develop(cudakernels)
 end
 Pkg.build()
 Pkg.precompile()
