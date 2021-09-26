5a72fb2a48aec6a69f9560060b0db655664e8134
Valentin Churavy <v.churavy@gmail.com>
2021-09-26 16:47:14 -0400

    on buildkite only dev&test the relevant packages

--- .ci/develop.jl
+++ .ci/develop.jl
@@ -6,13 +6,22 @@ root_directory = dirname(@__DIR__)
 
 kernelabstractions = Pkg.PackageSpec(path = root_directory)
 
+BACKEND = get(ENV, "KERNELABSTRACTIONS_TEST_BACKEND", "all")
+BUILDKITE = parse(Bool, get(ENV, "BUILDKITE", "false"))
+
+@info "Develop..." BUILDKITE
+
 Pkg.develop(kernelabstractions)
 if !(VERSION < v"1.6-")
-    rockernels = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "ROCKernels"))
-    Pkg.develop(rockernels)
+    if !BUILDKITE  || BACKEND == "ROCM"
+        rockernels = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "ROCKernels"))
+        Pkg.develop(rockernels)
+    end
 
-    cudakernels = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "CUDAKernels"))
-    Pkg.develop(cudakernels)
+    if !BUILDKITE  || BACKEND == "CUDA"
+        cudakernels = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "CUDAKernels"))
+        Pkg.develop(cudakernels)
+    end
 
     kernelgradients = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "KernelGradients"))
     Pkg.develop(kernelgradients)
