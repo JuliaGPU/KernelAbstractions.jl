4ea16ce89102cc479c14250a1fc52042fb748c8b
Navid C. Constantinou <navidcy@users.noreply.github.com>
2022-10-17 21:29:05 +0200

    fix rendering for @unroll macro docstring

--- src/extras/loopinfo.jl
+++ src/extras/loopinfo.jl
@@ -33,7 +33,7 @@ function loopinfo(expr, nodes...)
 end
 
 """
-   @unroll expr
+    @unroll expr
 
 Takes a for loop as `expr` and informs the LLVM unroller to fully unroll it, if
 it is safe to do so and the loop count is known.
