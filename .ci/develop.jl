bc0e91cc23eb83a1de29cdcf615b02dcaa2d70af
Tim Gymnich <timgymnich@me.com>
2022-11-21 11:37:45 -0500

    Enable macOS CI

--- .ci/develop.jl
+++ .ci/develop.jl
@@ -28,6 +28,11 @@ if !CI || BACKEND == "oneAPI"
     push!(pkgs, oneapikernels)
 end
 
+if !CI || BACKEND == "Metal"
+    metalkernels = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "MetalKernels"))
+    push!(pkgs, metalkernels)
+end
+
 if VERSION < v"1.8"
     kernelgradients = Pkg.PackageSpec(path = joinpath(root_directory, "lib", "KernelGradients"))
     push!(pkgs, kernelgradients)
