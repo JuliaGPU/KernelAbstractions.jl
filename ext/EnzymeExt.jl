ca138be420925f08e1e6d7564d85322e84f5cc20
Valentin Churavy <v.churavy@gmail.com>
2023-02-07 22:46:57 -0500

    flesh out proto-type

--- ext/EnzymeExt.jl
+++ ext/EnzymeExt.jl
@@ -1,6 +1,67 @@
 module EnzymeExt
-    using EnzymeCore: EnzymeRules
-    import KernelAbstractions
+    using EnzymeCore
+    using EnzymeCore.EnzymeRules
+    import KernelAbstractions: Kernel, wait
 
-    # TODO add rules
+    #TODO: Keywords are the worst
+
+    function EnzymeRules.augmented_primal(config::Config, func::Const{<:Kernel}, ::Duplicated{Event}, args...) where Event
+        @show config
+        @show func.val
+        @show args
+        @show Event
+
+        # Pre-allocate tape according to ndrange... * inner_tape
+        # kernel = func.val
+        # f = kernel.f
+        # function df(ctx, args...)
+        #     Enzyme.autodiff_deferred(Split, f, Enzyme.Const, ctx, args...)
+        #     return nothing
+        # end
+        # kernel' = similar(kernel, df)
+
+        # TODO autodiff_deferred on the func.val
+        forward, reverse = thunk(...)
+
+        subtape = nothing
+        if EnzymeRules.needs_primal(config)
+            primal, subtape = forward(args...)
+        else
+            primal = nothing
+        end
+
+        tape = Ref{Event}()
+        if EnzymeRules.needs_shadow(config)
+            function reverse_launch()
+                tape[] = reverse(args..., subtape)
+            end
+            shadow = reverse_launch
+        else
+            shadow = nothing
+        end
+        return AugmentedReturnFlexShadow(primal, shadow, tape)
+    end
+
+    function reverse(::Config, ::Const{<:Kernel}, ::Type{<:Const}, tape, args...)
+        wait(tape[])
+        return ()
+    end
+
+    function EnzymeRules.augmented_primal(config::Config, func::Const{typeof(wait)}, RT, args...)
+        if EnzymeRules.needs_primal(config)
+            primal = func.val(args...)
+        else
+            primal = nothing
+        end
+        return AugmentedReturn(primal, nothing, nothing)
+    end
+
+    function reverse(::Config, ::Const{typeof(wait)}, ::Type{<:Const}, tape, arg)
+        closure = cast(fn, arg.dval)
+        ev = closure()
+
+        wait(rev)
+
+        return ()
+    end
 end
