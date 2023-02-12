275a13503c53e96e3f2a7c961bc9b741c3801798
Valentin Churavy <v.churavy@gmail.com>
2023-02-12 17:17:30 -0500

    fixup! Nix requires

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,13 +8,14 @@ backend = get(ENV, "KERNELABSTRACTIONS_TEST_BACKEND", "CPU")
 if backend == "CPU"
     struct CPUDeviceArray{T,N,A} end # Fake and unused
     Testsuite.testsuite(CPU, backend, Base, Array, CPUDeviceArray)
-elseif backend = "CUDA"
+elseif backend == "CUDA"
     using CUDA
     if CUDA.functional()
-    CUDA.versioninfo()
-    CUDA.allowscalar(false)
-    Testsuite.testsuite(CUDADevice{false, false}, backend, CUDA, CuArray, CUDA.CuDeviceArray)
-    for (PreferBlocks, AlwaysInline) in Iterators.product((true, false), (true, false))
-        Testsuite.unittest_testsuite(CUDADevice{true, false}, backend, CUDA, CuArray, CUDA.CuDeviceArray)
+        CUDA.versioninfo()
+        CUDA.allowscalar(false)
+        Testsuite.testsuite(CUDADevice{false, false}, backend, CUDA, CuArray, CUDA.CuDeviceArray)
+        for (PreferBlocks, AlwaysInline) in Iterators.product((true, false), (true, false))
+            Testsuite.unittest_testsuite(CUDADevice{true, false}, backend, CUDA, CuArray, CUDA.CuDeviceArray)
+        end
     end
 end
