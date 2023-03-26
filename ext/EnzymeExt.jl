bedfe720722420c434b0940ffccb5ebbe1590a78
Valentin Churavy <v.churavy@gmail.com>
2023-03-26 15:54:13 -0400

    update for KA 0.9

--- ext/EnzymeExt.jl
+++ ext/EnzymeExt.jl
@@ -1,14 +1,8 @@
 module EnzymeExt
     using EnzymeCore
-    using Enzyme
     using EnzymeCore.EnzymeRules
-    import KernelAbstractions: Kernel, wait, StaticSize, launch_config, __groupsize, __groupindex, blocks, mkcontext, CPUEvent, CPU, construct
+    import KernelAbstractions: Kernel, StaticSize, launch_config, __groupsize, __groupindex, blocks, mkcontext
 
-    #TODO: Keywords are the worst
-    #
-    EnzymeRules.inactive(::typeof(construct), x...) = nothing
-    
-    EnzymeRules.inactive(::typeof(Core._compute_sparams), x...) = nothing
     EnzymeRules.inactive(::typeof(StaticSize), x...) = nothing
 
     function first_type(::Type{NamedTuple{A, B}}) where {A,B}
@@ -18,7 +12,7 @@ module EnzymeExt
         T.parameters[1]
     end
 
-    function EnzymeRules.augmented_primal(config::Config, func::Const{<:Kernel}, ::Event, args...; ndrange = nothing, workgroupsize = nothing, dependencies = nothing, progress = nothing) where Event
+    function EnzymeRules.augmented_primal(config::Config, func::Const{<:Kernel}, ::Nothing, args...; ndrange = nothing, workgroupsize = nothing)
         # Pre-allocate tape according to ndrange... * inner_tape
         kernel = func.val
         f = kernel.f
@@ -29,19 +23,19 @@ module EnzymeExt
         ctx = mkcontext(kernel, block, ndrange, iterspace, dynamic)
         ctxTy = typeof(ctx) # Base._return_type(mkcontext, Tuple{typeof(kernel), typeof(block), typeof(ndrange), typeof(iterspace), typeof(dynamic)})
         tt′ = Tuple{Const{ctxTy}, map(Core.typeof, args)...}
-        
+
         # TODO autodiff_deferred on the func.val
         ModifiedBetween = Val((overwritten(config)[1], false, overwritten(config)[2:end]...))
         forward, reverse = Enzyme.Compiler.thunk(f, #=df=#nothing, Const{Nothing}, tt′,  Val(Enzyme.API.DEM_ReverseModePrimal), Val(width(config)), ModifiedBetween)
         TapeType = first_type(Base._return_type(forward, tt′))
 
         subtape = Array{TapeType}(undef, __groupsize(ctx))
-        
+
         function fwd(ctx, subtape, args...)
             subtape[__groupindex(ctx)] = forward(Const(ctx), args...)[1]
             return nothing
         end
-        
+
         function rev(ctx, subtape, args...)
             reverse(Const(ctx), args..., subtape[__groupindex(ctx)])
             return nothing
@@ -50,39 +44,16 @@ module EnzymeExt
         aug_kernel = similar(kernel, fwd)
         rev_kernel = similar(kernel, rev)
 
-        primal = aug_kernel(subtape, args...; ndrange, workgroupsize, dependencies, progress)::CPUEvent
+        aug_kernel(subtape, args...; ndrange, workgroupsize)
 
-        tape = Ref{CPUEvent}()
-        @assert dependencies === nothing
-        function reverse_launch()
-            # TODO dependencies ?
-            tape[] = rev_kernel(subtape, args...; ndrange, workgroupsize, progress)
-            nothing
-        end
-        if !EnzymeRules.needs_primal(config)
-            primal = nothing
-        end
-        shadow = CPUEvent(Task(reverse_launch))
-        return AugmentedReturn(primal, shadow, tape)
+        tape = (; rev_kernel, subtape)
+
+        return AugmentedReturn(nothing, nothing, tape)
     end
 
-    function EnzymeRules.reverse(::Config, ::Const{<:Kernel}, ::Type{<:EnzymeCore.Annotation}, tape, args...)
-        wait(tape[])
-        return ()
-    end
-
-    function EnzymeRules.augmented_primal(config::Config, func::Const{typeof(wait)}, RT, ::Const{CPU}, arg::Duplicated{CPUEvent})
-        primal = func.val(arg.val)
-        if !EnzymeRules.needs_primal(config)
-            primal = nothing
-        end
-        return AugmentedReturn(primal, nothing, nothing)
-    end
-
-    function EnzymeRules.reverse(::Config, ::Const{typeof(wait)}, ::Type{<:Const}, tape, ::Const{CPU}, arg::Duplicated{CPUEvent})
-        # This duplicated is invalid, it contains the fake shadow
-        closure = arg.dval.task.code
-        closure()
+    function EnzymeRules.reverse(::Config, ::Const{<:Kernel}, ::Type{<:EnzymeCore.Annotation}, tape, args...; ndrange=nothing, workgroupsize=nothing)
+        rev_kernel, subtape = tape...
+        rev_kernel(subtape, args...; ndrange, workgroupsize)
         return ()
     end
 end
