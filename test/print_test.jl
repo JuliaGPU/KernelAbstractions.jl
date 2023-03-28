85108694d5cb4f9fe1c62452a487aa82c38dc733
Tim Besard <tim.besard@gmail.com>
2023-03-28 10:21:35 +0200

    Silence printing test suite.

--- test/print_test.jl
+++ test/print_test.jl
@@ -6,13 +6,21 @@ using KernelAbstractions, Test
 end
 
 function printing_testsuite(backend)
+    # TODO: capture output and verify it (this requires the ability to check for
+    #       back-end capabilities, as not all back-ends support printing)
+
     @testset "print test" begin
         kernel = kernel_print(backend(), 4)
-        kernel(ndrange=(4,))
-        synchronize(backend())
+        redirect_stdout(devnull) do
+          kernel(ndrange=(4,))
+          synchronize(backend())
+        end
         @test true
 
-        @print("Why this should work\n")
+        redirect_stdout(devnull) do
+          @print("Why this should work\n")
+          synchronize(backend())
+        end
         @test true
     end
 end
