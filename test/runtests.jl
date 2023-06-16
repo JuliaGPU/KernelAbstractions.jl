e10f9a03133298c312bf5f490dfd031d9405ac89
Valentin Churavy <v.churavy@gmail.com>
2023-06-16 13:42:51 -0400

    Add implementation notes to host functionality

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,3 +20,30 @@ kern_static(CPU(static=true), (1,))(A, ndrange=length(A))
 @kernel cpu=false function my_no_cpu_kernel(a)
 end
 @test_throws ErrorException("This kernel is unavailable for backend CPU") my_no_cpu_kernel(CPU())
+
+struct NewBackend <: KernelAbstractions.GPU end
+@testset "Default host implementation" begin
+    backend = NewBackend()
+    @test KernelAbstractions.isgpu(backend) == true
+
+    @test_throws MethodError KernelAbstractions.synchronize(backend)
+
+    @test_throws MethodError KernelAbstractions.allocate(backend, Float32, 1)
+    @test_throws MethodError KernelAbstractions.allocate(backend, Float32, (1,))
+    @test_throws MethodError KernelAbstractions.allocate(backend, Float32, 1, 2)
+
+    @test_throws MethodError KernelAbstractions.zeros(backend, Float32, 1)
+    @test_throws MethodError KernelAbstractions.ones(backend, Float32, 1)
+
+    @test KernelAbstractions.supports_atomics(backend) == true
+    @test KernelAbstractions.supports_float64(backend) == true
+
+    @test KernelAbstractions.priority!(backend, :high) === nothing
+    @test KernelAbstractions.priority!(backend, :normal) === nothing
+    @test KernelAbstractions.priority!(backend, :low) === nothing
+
+    @test_throws ErrorException KernelAbstractions.priority!(backend, :middle)
+
+    kernel = my_no_cpu_kernel(backend)
+    @test_throws MethodError kernel()
+end
