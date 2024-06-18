d8516891ba0a4ccd07c6ac165c5f72cfc96ce877
William Moses <gh@wsmoses.com>
2024-06-18 16:37:41 -0400

    Update utils.jl

--- examples/utils.jl
+++ examples/utils.jl
@@ -1,5 +1,5 @@
 # EXCLUDE FROM TESTING
-if backend_str == "CUDA" && Base.find_package("CUDA") !== nothing
+if Base.find_package("CUDA") !== nothing
     using CUDA
     using CUDA.CUDAKernels
     const backend = CUDABackend()
