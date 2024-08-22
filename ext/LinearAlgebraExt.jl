91ada95c10c2b3852839fec162cb64eea74fe3f6
Valentin Churavy <vchuravy@users.noreply.github.com>
2024-08-22 13:49:41 +0200

    Make stdlib extensions backwards compatible (#512)

--- ext/LinearAlgebraExt.jl
+++ ext/LinearAlgebraExt.jl
@@ -1,11 +1,7 @@
 module LinearAlgebraExt
 
 using KernelAbstractions: KernelAbstractions
-if isdefined(Base, :get_extension)
-    using LinearAlgebra: Tridiagonal, Diagonal
-else
-    using ..LinearAlgebra: Tridiagonal, Diagonal
-end
+using LinearAlgebra: Tridiagonal, Diagonal
 
 KernelAbstractions.get_backend(A::Diagonal) = KernelAbstractions.get_backend(A.diag)
 KernelAbstractions.get_backend(A::Tridiagonal) = KernelAbstractions.get_backend(A.d)
--- ext/SparseArraysExt.jl
+++ ext/SparseArraysExt.jl
@@ -1,11 +1,7 @@
 module SparseArraysExt
 
 using KernelAbstractions: KernelAbstractions
-if isdefined(Base, :get_extension)
-    using SparseArrays: AbstractSparseArray, rowvals
-else
-    using ..SparseArrays: AbstractSparseArray, rowvals
-end
+using SparseArrays: AbstractSparseArray, rowvals
 
 function KernelAbstractions.get_backend(A::AbstractSparseArray)
     return KernelAbstractions.get_backend(rowvals(A))
