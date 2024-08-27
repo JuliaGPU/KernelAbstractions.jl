f0c18d9af221e294626fa6f2caf8c93ab301ba47
Nick OBrien <nick4f42@proton.me>
2024-08-26 21:19:29 -0500

    Fix zeros erroring on an empty shape
    Fixes #504.
    
    8b94cc693a970b3cd675f827c99b3311d06d0aec introduced this bug.

--- src/cpu.jl
+++ src/cpu.jl
@@ -49,7 +49,10 @@ end
 const CPU_GRAINSIZE = 1024 # Vectorization, 4x unrolling, minimal grain size
 function default_cpu_workgroupsize(ndrange)
     # if the total kernel is small, don't launch multiple tasks
-    if prod(ndrange) <= CPU_GRAINSIZE
+    n = prod(ndrange)
+    if iszero(n)
+        return map(one, ndrange)
+    elseif n <= CPU_GRAINSIZE
         return ndrange
     else
         available = Ref(CPU_GRAINSIZE)
