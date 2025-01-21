825822ec46febf177a6deacfa791e761d2bcf0a9
Valentin Churavy <vchuravy@users.noreply.github.com>
2025-01-21 14:04:43 +0100

    Apply suggestions from code review

--- test/devices.jl
+++ test/devices.jl
@@ -1,7 +1,7 @@
 function devices_testsuite(Backend)
     backend = Backend()
 
-    current_device = KernelAbstractions.device!(backend)
+    current_device = KernelAbstractions.device(backend)
     for i in KernelAbstractions.ndevices(backend)
         KernelAbstractions.device!(backend, i)
         @test KernelAbstractions.device(backend) == i
@@ -9,6 +9,6 @@ function devices_testsuite(Backend)
 
     @test_throws ArgumentError KernelAbstractions.device!(backend, 0)
     @test_throws ArgumentError KernelAbstractions.device!(backend, KernelAbstractions.ndevices(backend) + 1)
-    return KernelAbstractions.device!(backend, current_device)
     KernelAbstractions.device!(backend, current_device)
+    return nothing
 end
