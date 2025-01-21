0f80d392d7ed7915ea8a7d2b55a51abe8f974364
Valentin Churavy <v.churavy@gmail.com>
2025-01-21 13:41:15 +0100

    Add tests

---
+++ test/devices.jl
@@ -0,0 +1,14 @@
+function devices_testsuite(Backend)
+    backend = Backend()
+
+    current_device = KernelAbstractions.device!(backend)
+    for i in KernelAbstractions.ndevices(backend)
+        KernelAbstractions.device!(backend, i)
+        @test KernelAbstractions.device(backend) == i
+    end
+
+    @test_throws ArgumentError KernelAbstractions.device!(backend, 0)
+    @test_throws ArgumentError KernelAbstractions.device!(backend, KernelAbstractions.ndevices(backend)+1)
+
+    KernelAbstractions.device!(backend, current_device)
+end
