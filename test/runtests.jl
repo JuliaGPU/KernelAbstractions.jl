ff726d00e1de323c984383b3323c193ff3a0ba87
Valentin Churavy <v.churavy@gmail.com>
2025-03-12 10:29:15 +0100

    Remove old CPU test (#580)

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,32 +10,6 @@ include("testsuite.jl")
     Testsuite.testsuite(CPU, "CPU", Base, Array, CPUBackendArray)
 end
 
-@kernel function kern_static(A)
-    I = @index(Global)
-    A[I] = Threads.threadid()
-end
-
-A = zeros(Int, Threads.nthreads())
-kern_static(CPU(static = true), (1,))(A, ndrange = length(A))
-@test A == 1:Threads.nthreads()
-
-if Base.JLOptions().check_bounds == 0 || Base.JLOptions().check_bounds == 1
-    # testing bounds errors
-    @kernel inbounds = false function my_bounded_kernel(a)
-        idx = @index(Global, Linear)
-        a[idx] = 0
-    end
-    @test_throws BoundsError(Int64[], (1,)) my_bounded_kernel(CPU())(Int[], ndrange = 1)
-end
-
-if Base.JLOptions().check_bounds == 0 || Base.JLOptions().check_bounds == 2
-    @kernel inbounds = true function my_bounded_kernel(a)
-        idx = @index(Global, Linear)
-        a[idx] = 0
-    end
-    @test nothing == my_inbounds_kernel(CPU())(Int[], ndrange = 1)
-end
-
 struct NewBackend <: KernelAbstractions.GPU end
 @testset "Default host implementation" begin
     backend = NewBackend()
@@ -60,9 +34,6 @@ struct NewBackend <: KernelAbstractions.GPU end
     @test_throws ErrorException KernelAbstractions.priority!(backend, :middle)
 
     @test KernelAbstractions.functional(backend) === missing
-
-    kernel = my_no_cpu_kernel(backend)
-    @test_throws MethodError kernel()
 end
 
 
