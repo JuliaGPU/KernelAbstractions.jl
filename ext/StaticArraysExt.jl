15f54e6e37d224b9dae7d15d0a877ba5ba90b82b
Valentin Churavy <v.churavy@gmail.com>
2025-06-26 17:01:44 +0200

    Remove SArray import

--- ext/StaticArraysExt.jl
+++ ext/StaticArraysExt.jl
@@ -1,11 +1,9 @@
 module StaticArraysExt
 
 import KernelAbstractions: get_backend, CPU
-using StaticArrays: SizedArray, MArray, SArray
+using StaticArrays: SizedArray, MArray
 
 get_backend(A::SizedArray) = get_backend(A.data)
 get_backend(::MArray) = CPU()
-# TODO: It makes sense to pass SArray to the GPU backend, so we can't make a determination
-# get_backend(::SArray) = CPU()
 
 end
