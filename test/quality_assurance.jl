94d9c677e57f0fce8ff6123ec1318a7cd1935f64
Mosè Giordano <m.giordano@ucl.ac.uk>
2026-06-11 11:20:42 +0100

    Ignore Enzyme/EnzymeCore stale deps
    They're manually installed in the CI jobs.

--- test/quality_assurance.jl
+++ test/quality_assurance.jl
@@ -2,7 +2,10 @@ using Aqua
 
 function quality_assurance_testsuite()
     @testset "Aqua" begin
-        Aqua.test_all(KernelAbstractions)
+        Aqua.test_all(
+            KernelAbstractions;
+            stale_deps = (; ignore = [:Enzyme, :EnzymeCore]),
+        )
     end
     return nothing
 end
