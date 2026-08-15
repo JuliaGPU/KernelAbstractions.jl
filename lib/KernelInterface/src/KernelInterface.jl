5dfcc0113cbb2505369ebde74d5366e4fa45a034
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-08-15 12:46:56 +0200

    Move macro utils to own file

--- lib/KernelInterface/src/KernelInterface.jl
+++ lib/KernelInterface/src/KernelInterface.jl
@@ -12,70 +12,7 @@ like allocating arrays on a backend.
 """
 module KernelInterface
 
-## macro tools
-# Vendored from GPUCompiler to keep KernelInterface free of heavy dependencies.
-
-# split keyword arguments expressions into groups. returns vectors of keyword argument
-# values, one more than the number of groups (unmatched keywords in the last vector).
-# intended for use in macros; the resulting groups can be used in expressions.
-# can be used at run time, but not in performance critical code.
-function split_kwargs(kwargs, kw_groups...)
-    kwarg_groups = ntuple(_ -> [], length(kw_groups) + 1)
-    for kwarg in kwargs
-        # decode
-        if Meta.isexpr(kwarg, :(=))
-            # use in macros
-            key, val = kwarg.args
-        elseif kwarg isa Pair{Symbol, <:Any}
-            # use in functions
-            key, val = kwarg
-        else
-            throw(ArgumentError("non-keyword argument like option '$kwarg'"))
-        end
-        isa(key, Symbol) || throw(ArgumentError("non-symbolic keyword '$key'"))
-
-        # find a matching group
-        group = length(kwarg_groups)
-        for (i, kws) in enumerate(kw_groups)
-            if key in kws
-                group = i
-                break
-            end
-        end
-        push!(kwarg_groups[group], kwarg)
-    end
-
-    return kwarg_groups
-end
-
-# assign arguments to variables, handle splatting
-function assign_args!(code, _args)
-    nargs = length(_args)
-
-    # handle splatting
-    splats = Vector{Bool}(undef, nargs)
-    args = Vector{Any}(undef, nargs)
-    for i in 1:nargs
-        splats[i] = Meta.isexpr(_args[i], :(...))
-        args[i] = splats[i] ? _args[i].args[1] : _args[i]
-    end
-
-    # assign arguments to variables
-    vars = Vector{Symbol}(undef, nargs)
-    for i in 1:nargs
-        vars[i] = gensym()
-        push!(code.args, :($(vars[i]) = $(args[i])))
-    end
-
-    # convert the arguments, compile the function and call the kernel
-    # while keeping the original arguments alive
-    var_exprs = Vector{Any}(undef, nargs)
-    for i in 1:nargs
-        var_exprs[i] = splats[i] ? Expr(:(...), vars[i]) : vars[i]
-    end
-
-    return vars, var_exprs
-end
+include("utils.jl")
 
 """
     get_global_size()::@NamedTuple{x::Int, y::Int, z::Int}
